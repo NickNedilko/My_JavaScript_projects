94de0b870d05c54bfa9475ef17b4dab7f4dec9b6
NickNedilko <overlord311089@gmail.com>
2022-09-24 10:31:14 +0300

    clock javaScript

--- js/clock.js
+++ js/clock.js
@@ -7,15 +7,13 @@ const secondHand = document.querySelector("[data-second-hand]");
 function setClock() {
   const currentDate = new Date();
   const secondsRatio = currentDate.getSeconds() / 60;
-  const minutesRatio = (seconds + currentDate.getMinutes()) / 60;
-  const hoursRatio = (minutes + currentDate.getHours()) / 12;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  console.log(minutesRatio);
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
 }
-setRotation(secondHand, secondsRatio);
-setRotation(minuteHand, minutesRatio);
-setRotation(hourHand, hoursRatio);
-
 function setRotation(element, rotationRatio) {
   element.style.setProperty("--rotation", rotationRatio * 360);
 }
-
-setClock();
