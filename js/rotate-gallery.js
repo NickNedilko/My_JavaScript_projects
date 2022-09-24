925d83fe26bdaa5d7bdb16dbaeb01e2eb7d0bb9b
NickNedilko <overlord311089@gmail.com>
2022-09-24 14:13:59 +0300

    rotate gallery

---
+++ js/rotate-gallery.js
@@ -0,0 +1,31 @@
+const imageContainerEl = document.querySelector(".image-container");
+
+const prevEl = document.querySelector(".btn1");
+const nextEl = document.querySelector(".btn2");
+let x = 0;
+let timer;
+console.log(timer)
+prevEl.addEventListener("click", () => {
+  x = x + 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+nextEl.addEventListener("click", () => {
+  x = x - 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+function updateGallery() {
+  imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+  timer = setTimeout(() => {
+    x = x - 45;
+    updateGallery();
+  }, 3000);
+}
+
+updateGallery();
+
+
+
