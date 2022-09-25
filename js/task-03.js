c780e41a0c6e06045d3ed0089c44de8ecca86397
NickNedilko <overlord311089@gmail.com>
2022-09-25 16:04:53 +0300

    changes

--- js/task-03.js
+++ js/task-03.js
@@ -1,30 +1,30 @@
-const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
+const starsEl = document.querySelectorAll('.fa-star');
+const emojisEl = document.querySelectorAll('.far');
+const bodyEl = document.querySelector('body');
+const colors = ['red', 'tomato', 'orange', 'lightgreen', 'green']
 
-const newLiEl = images.map(element => {
-  const liEl = document.createElement('li')
-  const  imgEl = document.createElement('img');
-  imgEl.src = element.url;
-  imgEl.alt = element.alt;
-  imgEl.classList.add('list__img');
-  liEl.classList.add('list__item')
+starsEl.forEach((starEl, index)=> {
+  starEl.addEventListener('click', () =>{
+     updateRating(index)
+  });
+});
 
-  liEl.appendChild(imgEl);
-  return liEl;
-})
-const ulEl = document.querySelector('.gallery');
-ulEl.classList.add('list');
-ulEl.append(...newLiEl);
+function updateRating(index){
+  starsEl.forEach((starEl,idx)=>{
+     if(idx < index + 1){
+      starEl.classList.add('active');
+      starEl.style.color = colors[index]
+     }else{
+      starEl.classList.remove('active')
+      starEl.style.color = ''
+     } 
+
+     emojisEl.forEach((emojiEl )=>{
+      emojiEl.style.transform = `translateX(-${index*50}px)`;
+      emojiEl.style.color = colors[index]
+      bodyEl.style.background = colors[index]
+     })
+
+  })
+} 
 
--- js/task-04.js
+++ js/task-04.js
@@ -1,33 +1,48 @@
-const btnIncrement = document.querySelector(
-  "#counter button[data-action='increment']"
-);
-const btnDecrement = document.querySelector(
-  "#counter button[data-action='decrement']"
-);
-const counter = document.querySelector("#value");
+const yearEl = document.querySelector('.year')
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
 
-let counterValue = 0;
-console.log(typeof counter.textContent);
-btnDecrement.addEventListener("click", () => {
-  counterValue -= 1;
-  counter.textContent = counterValue;
-});
+const newYearTime = new Date(`Jan 1, ${yearEl.textContent} 00:00:00`).getTime();
+const btnIncrease = document.querySelector('.increase')
+const btnDecrease = document.querySelector('.decrease')
 
-btnIncrement.addEventListener("click", () => {
-  counterValue += 1;
-  counter.textContent = counterValue;
-});
 
-// =================================2==================================
-// btnDecrement.addEventListener('click', onBtnDecrementClick)
-// btnIncrement.addEventListener('click', onBtnIncrementClick)
+console.log(newYearTime)
+btnIncrease.addEventListener('click', ()=>{
 
-//   function onBtnDecrementClick(){
-//     counterValue -= 1;
-//     counter.textContent = counterValue;
-//   }
+  yearEl.textContent = Number(yearEl.textContent) +1;
+  updateCountdown()
+  })
+  
+  btnDecrease.addEventListener('click', ()=>{
+  
+    yearEl.textContent -= 1;
+    updateCountdown()
+    
+    })
+  
 
-//   function onBtnIncrementClick(){
-//     counterValue += 1;
-//     counter.textContent = counterValue;
-//   }
+updateCountdown();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const gap = newYearTime - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+  dayEl.innerText = d;
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  setTimeout(updateCountdown, 1000)
+}
+
