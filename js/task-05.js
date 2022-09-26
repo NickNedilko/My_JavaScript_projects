26e01599362df378f5b848ca9371f55cf55217b4
NickNedilko <overlord311089@gmail.com>
2022-09-26 18:08:27 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -7,40 +7,57 @@ btnEl.addEventListener("click", () => {
   createPassword();
 });
 
-// copyIconEl.addEventListener("click", () => {
-//   copyPassword();
-//   if (inputEl.value) {
-//     alertContainerEl.classList.remove("active");
-//     setTimeout(() => {
-//       alertContainerEl.classList.add("active");
-//     }, 2000);
-//   }
-// });
+copyIconEl.addEventListener("click", () => {
+  copyPassword();
+  if (inputEl.value) {
+    alertContainerEl.classList.remove("active");
+    setTimeout(() => {
+      alertContainerEl.classList.add("active");
+    }, 2000);
+  }
+});
 
 
-const AlfSmallArray = [];
-const big = 10;
- for (let index = 1; index <=10; index++){
-    AlfSmallArray.push(i);
+function makeSrting(){}
+
+const alfSmallArray = [];
+ for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++){
+    let ad = String.fromCharCode(i);
+    alfSmallArray.push(ad);
  } 
-console.log(AlfSmallArray);
+ const alfBigArray = alfSmallArray.join('').toUpperCase().split('')
+
+const symbolArray = [];
+for (let i = '!'.charCodeAt(0); i <= '@'.charCodeAt(0); i++){
+  const validNumber = Number(String.fromCharCode(i))
+  if((Number.isNaN(validNumber))){
+    let ad = String.fromCharCode(i);
+    symbolArray.push(ad);
+  }
+ 
+} 
+
+console.log(symbolArray)
+
+const charArray = [...alfBigArray, ...alfSmallArray,...symbolArray]
+const chars = charArray.join('');
+console.log(chars);
 
 function createPassword() {
-  const chars =
-    "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const passwordLength = 14;
   let password = "";
   for (let index = 0; index < passwordLength; index++) {
     const randomNum = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNum, randomNum + 1);
+    console.log(randomNum);
+    console.log(password)
   }
   inputEl.value = password;
   alertContainerEl.innerText = password + " copied!";
 }
 
-// function copyPassword() {
-//   inputEl.select();
-//   inputEl.setSelectionRange(0, 9999);
-//   navigator.clipboard.writeText(inputEl.value);
-// }
-
+function copyPassword() {
+  inputEl.select();
+  inputEl.setSelectionRange(0, 9999);
+  navigator.clipboard.writeText(inputEl.value);
+}
