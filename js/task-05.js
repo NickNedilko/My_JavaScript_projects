404039f4baada62858ff5020e517621c500c01f6
NickNedilko <overlord311089@gmail.com>
2022-09-26 13:16:21 +0300

    zXcx

--- js/task-05.js
+++ js/task-05.js
@@ -1,17 +1,46 @@
-const inputEl = document.querySelector('#name-input');
-const nameEl = document.querySelector('#name-output');
+const btnEl = document.querySelector(".btn");
+const inputEl = document.getElementById("input");
+const copyIconEl = document.querySelector(".fa-copy");
+const alertContainerEl = document.querySelector(".alert-container");
 
-inputEl.addEventListener ('input', () =>{
-    nameEl.textContent = (inputEl.value === '') ? nameEl.textContent = "Anonymous" : nameEl.textContent = inputEl.value
-})
+btnEl.addEventListener("click", () => {
+  createPassword();
+});
 
-// =============================================================================================================
-// inputEl.addEventListener ('input', () =>{
-//     if(inputEl.value === ''){
-//         nameEl.textContent = "Anonymous"
-//     }
-//     else{
-//         nameEl.textContent = inputEl.value
-//     }
-// })
+// copyIconEl.addEventListener("click", () => {
+//   copyPassword();
+//   if (inputEl.value) {
+//     alertContainerEl.classList.remove("active");
+//     setTimeout(() => {
+//       alertContainerEl.classList.add("active");
+//     }, 2000);
+//   }
+// });
+
+
+const AlfSmallArray = [];
+const big = 10;
+ for (let index = 1; index <=10; index++){
+    AlfSmallArray.push(i);
+ } 
+console.log(AlfSmallArray);
+
+function createPassword() {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const passwordLength = 14;
+  let password = "";
+  for (let index = 0; index < passwordLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNum, randomNum + 1);
+  }
+  inputEl.value = password;
+  alertContainerEl.innerText = password + " copied!";
+}
+
+// function copyPassword() {
+//   inputEl.select();
+//   inputEl.setSelectionRange(0, 9999);
+//   navigator.clipboard.writeText(inputEl.value);
+// }
 
