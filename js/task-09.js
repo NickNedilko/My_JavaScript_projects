e7c282af361095456277eee259c360ad9627f278
NickNedilko <overlord311089@gmail.com>
2022-10-02 10:47:17 +0300

    xzddf

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
-const btnColorChangerEl = document.querySelector('.change-color')
-const bodyEl = document.querySelector('body')
-const spanEl = document.querySelector('.color')
+// const btnColorChangerEl = document.querySelector('.change-color')
+// const bodyEl = document.querySelector('body')
+// const spanEl = document.querySelector('.color')
 
-btnColorChangerEl.addEventListener('click', onBtnClick)
+// btnColorChangerEl.addEventListener('click', onBtnClick)
 
-function onBtnClick (){
-bodyEl.style.backgroundColor = getRandomHexColor();
-spanEl.textContent = bodyEl.style.backgroundColor;
-}
+// function onBtnClick (){
+// bodyEl.style.backgroundColor = getRandomHexColor();
+// spanEl.textContent = bodyEl.style.backgroundColor;
+// }
 
 console.log('hi world')
